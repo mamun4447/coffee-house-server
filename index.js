8c84fa31e74c73c87f29d58a229fe56466cb0277
mamun4447 <abdullah15-12557@diu.edu.bd>
2025-07-01 16:31:30 +0600

    verseljson

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //==>Middleware<==//
-// app.use(cors());
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
 //<------------------------------------>//
 //<===============MongoDB==============>//
